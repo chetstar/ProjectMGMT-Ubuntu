70333cb745b7c1843a929b8441209592996fee65
Chet <chetstar@gmail.com>
2015-05-22 18:28:15 -0700

    fixed db migrations

--- run.py
+++ run.py
@@ -2,6 +2,8 @@
 from app import app
 # app.run(host='0.0.0.0',debug=True)
 # app.run(debug = True)
-
+#!flask/bin/python
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=80)
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080,debug=True)
