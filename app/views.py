3d9fa2937f7b3975d4071fb6f451962535ddf27f
py <chetstar@gmail.com>
2015-11-02 10:02:59 -0800

    sleek new js form

--- app/views.py
+++ app/views.py
@@ -916,6 +916,7 @@ def tocquestion(id,action,sub):
     form_systems=TOCsystems()
     # import pdb;pdb.set_trace()
     if form.validate_on_submit():
+        action='test'
         if form.submitTOCquestion.data:
             new_q=models.dashboards_questions(question=form.question.data,dashboard_id=id)
             db.session.add(new_q)
@@ -947,7 +948,7 @@ def tocquestion(id,action,sub):
             db.session.delete(models.dashboards_systems.query.filter_by(id=sub).first())
             db.session.commit()
             toc_row=models.dashboards.query.filter_by(id=id).first()
-    return render_template("tocquestions.html",toc_row=toc_row,form=form,id=id,form_review=form_review,form_systems=form_systems)
+    return render_template("tocquestions.html",toc_row=toc_row,form=form,id=id,form_review=form_review,form_systems=form_systems,action=action)
 
 @app.route("/toclist",methods=["GET","POST"])
 @logged_in
